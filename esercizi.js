0be4aa56162f5a6f6a11753356392688ffc67d36
Aresso93 <ares.fiumicelli@gmail.com>
2023-08-15 17:36:43 +0200

    esercizio

--- esercizi.js
+++ esercizi.js
@@ -362,3 +362,28 @@ function findNextSquare(sq) {
     return -1;
     }
 }
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+// Write a method that takes the array as an argument and returns this "outlier" N.
+
+function findOutlier(integers){
+ 
+    const even = integers.filter(number => {
+        return number % 2 === 0;
+    })
+
+    const odd = integers.filter(number =>{
+        return number % 2 !== 0;
+    })
+
+    if (even.length <= 1) {
+        return even;
+    } else if (odd.length <= 1){
+        return odd;
+    } else {
+        return 'undefined'
+    }
+
+  
+ 
+  }
