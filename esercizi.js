f222f411ab90b701ccd12dd21846bbf5e8a52961
Aresso93 <ares.fiumicelli@gmail.com>
2023-08-17 21:29:29 +0200

    esercizi

--- esercizi.js
+++ esercizi.js
@@ -539,10 +539,34 @@ function toWeirdCase(string){
 
   console.log(toWeirdCase('faraoni gialli maledetti bastardi'));
 
-//   ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+//You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
-// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
-
-function rot13(message){
+function sortArray(array) {
     
+    let oddArray = [];
+    let evenArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const number = array[i];
+        
+        if(number % 2 === 0){
+
+            evenArray.push(number);
+
+        } else {
+            oddArray.push(number)
+        }
+
+    }
+
+    let orderedOddArray = oddArray.sort((a, b) => a - b);
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] %2 !== 0) {
+            array[i] = orderedOddArray[0];
+            orderedOddArray.shift()
+        }
+        
+    }
+    return array;
   }
+
