13dfd5c2f0cdb831da5fff9d7fbe2cd1626b2e78
Aresso93 <ares.fiumicelli@gmail.com>
2023-08-27 18:29:33 +0200

    esercizi

--- esercizi2.js
+++ esercizi2.js
@@ -309,3 +309,17 @@ console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12,
 function countPositivesSumNegatives(input) 
 
 //DA SISTEMARE 
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  
+}
